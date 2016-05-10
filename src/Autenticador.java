e2c1751c3e5baea663fc45886bb3c9d991a1c574
Emanuel Rocha <emanuelrm@gmail.com>
2016-05-09 22:46:59 -0400

    comit

--- src/Autenticador.java
+++ src/Autenticador.java
@@ -1,4 +1,12 @@
 
 public class Autenticador {
-
+	
+	public Usuario logar(String login, String senha) throws LoginException{
+		
+		if (login.equals("emanuel") && senha.equals("senha")) {
+			return new Usuario(login);
+		}
+		
+		throw new LoginException("Usuário ou senha incorretos.", login);
+	}
 }
