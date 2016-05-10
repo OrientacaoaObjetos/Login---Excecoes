d719b67f1adda45b8330b2acf648c5386cf14fac
Emanuel Rocha <emanuelrm@gmail.com>
2016-05-09 22:53:01 -0400

    cmt

--- src/TesteAutenticador.java
+++ src/TesteAutenticador.java
@@ -5,8 +5,28 @@ import org.junit.Test;
 public class TesteAutenticador {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void loginComSucesso() throws LoginException {
+		Autenticador autenticador = new Autenticador();
+		Usuario usuario = autenticador.logar("emanuel", "senha");
+		assertEquals("emanuel", usuario.getLogin());
+	}
+	
+	@Test(expected = LoginException.class)
+	public void loginFalha() throws LoginException {
+		Autenticador autenticador = new Autenticador();
+		Usuario usuario = autenticador.logar("emanuel", "senhaerrada");
+	}
+	
+	@Test
+	public void informacaoErro() throws LoginException {
+		Autenticador autenticador = new Autenticador();
+		try {
+			Usuario usuario = autenticador.logar("emanuel", "senhaerrada");
+			fail();
+			
+		} catch (LoginException e) {
+			assertEquals("emanuel", e.getLogin());
+		}
 	}
 
 }
